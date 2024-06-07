c20ecda0507301c60e90ca2ab825a3bfe61394f6
Fabio Cruz <fabinhocosta1202@gmail.com>
2024-06-07 19:25:18 -0300

    Add shadow on client card

--- src/components/clients/ClientCard.vue
+++ src/components/clients/ClientCard.vue
@@ -1,39 +1,63 @@
 <template>
   <q-card id="client-card" class="q-my-sm my-card">
-    <q-card-section>
-      <div class="text-h5 q-mt-sm q-mb-xs">
-        {{ client.name }}
-      </div>
-      <div class="text-overline">
-        <q-icon
-          class="text-positive"
-          name="headphones"
-          size="14px"
-          style="margin-top: -3px"
-        ></q-icon>
-        <a
-          class="text-positive"
-          :href="`https://wa.me/55${client.phone_number.replace(/\D/g, '')}`"
-          target="_blank"
-        >
-          {{ client.phone_number }}
-        </a>
-      </div>
-      <div class="text-overline">Desde: {{ $d(client.created_at) }}</div>
-      <div class="text-caption text-grey">
-        {{ client.description }}
-      </div>
-    </q-card-section>
-
-    <q-card-actions>
-      <q-btn color="info" icon="edit"></q-btn>
-      <q-btn color="negative" icon="delete"></q-btn>
-      <q-btn
-        color="accent"
-        icon="add"
-        @click="$emit('selectedclient', client)"
-      ></q-btn>
-    </q-card-actions>
+    <div class="row">
+      <q-card-section class="column justify-around items-center text-center">
+        <div class="q-mb-sm">
+          <q-icon
+            name="las la-user"
+            size="48px"
+            color="neutral-l_grey"
+          ></q-icon>
+          <div class="column">
+            <span class="text-neutral-l_grey text-body1">0</span>
+            <span style="font-size: 8px">PEDIDOS</span>
+          </div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h6 text-neutral-grey">
+          <span>{{ client.name }}</span>
+          <div class="column text-overline">
+            <div style="margin: -4px 0px -4px 0">
+              <q-icon
+                class="text-positive"
+                name="lab la-whatsapp"
+                size="14px"
+              ></q-icon>
+              <a
+                class="text-positive"
+                :href="`https://wa.me/55${client.phone_number.replace(
+                  /\D/g,
+                  ''
+                )}`"
+                target="_blank"
+              >
+                {{ client.phone_number }}
+              </a>
+            </div>
+            <span class="text-neutral-l_grey text-weight-light text-caption">{{
+              client.description
+            }}</span>
+          </div>
+          <div class="row q-gutter-xs">
+            <q-btn
+              color="info"
+              size="sm"
+              outline
+              no-caps
+              icon="las la-pencil-alt"
+            ></q-btn>
+            <q-btn
+              color="negative"
+              icon="lar la-trash-alt"
+              size="sm"
+              outline
+              no-caps
+            ></q-btn>
+          </div>
+        </div>
+      </q-card-section>
+    </div>
   </q-card>
 </template>
 
